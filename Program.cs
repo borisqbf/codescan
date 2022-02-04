38ec0518ecf1e0bf42f1c315ef6afccae1e7d71a
Boris <boris.klyachko@citywestwater.com.au>
2022-02-04 13:07:08 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codescan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
